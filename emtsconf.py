096b43c46c917970b2eb056a27415114722ee22a
bigvik <bigvikt@gmail.com>
2023-09-09 14:39:05 +0600

    Del config file

--- emtsconf.py
+++
@@ -1,6 +0,0 @@
-efrom = 'bigvikt@mail.ru' # Свой адрес почты
-eserver = 'smtp.mail.ru'
-eport = 465
-epass = 'ifDbMrFp5OhSCmbVZ46S' #Свой пароль приложения
-subj = 'Файлы'
-text = 'Файлы во вложении'
